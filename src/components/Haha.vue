cc446aed12431826766878843d062cab18d719ca
xiaop3344 <916632315@qq.com>
2019-05-29 21:59:36 +0800

    修改Eslint检查 联系main.js

---
+++ src/components/Haha.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p class="msg">{{msg}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg:'hahahah'
+        }
+    }
+}
+</script>
+<style>
+    .msg{
+        color: red;
+        background:aqua
+    }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+ import App from './App' 
+// import router from './router'
+import Haha from './components/Haha.vue'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  components: { Haha },
+  template: '<Haha/>'
 })
